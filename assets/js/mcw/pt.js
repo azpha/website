bd2ac88ed9eab709d42449caeec8a156a954c6b2
awex <alex@plaguecraft.xyz>
2021-06-03 01:30:47 -0400

    i am dumb lmfao pt hopefully fixed

--- assets/js/mcw/pt.js
+++ assets/js/mcw/pt.js
@@ -1,11 +1,11 @@
 async function postPlain() {
-    const link = document.getElementById("cliplink").value
+    const link = document.getElementById("cliplinkpt").value
     console.log('Link:', link)
-    const webhook = document.getElementById("webhookURL").value
+    const webhook = document.getElementById("webhookURLpt").value
     console.log('Webhook:', webhook)
-    const caption = document.getElementById("caption").value
+    const caption = document.getElementById("captionpt").value
     console.log('Caption:', caption)
-    const username = document.getElementById("medalUsername").value
+    const username = document.getElementById("medalUsernamept").value
     console.log('Username:', username)
 
     const { response } = await fetch(`https://mcwapi.herokuapp.com/clip-check-pt?clipLink=${link}&webhookURL=${webhook}&medalUsername=${username}&caption=${caption}`).then(response => response.json());
