5db374c2b3e7a73f1e39c0278f09c0e057a24158
awex <alex@plaguecraft.xyz>
2021-06-21 12:09:23 -0400

    added screenshot js file

---
+++ assets/js/screenshot.js
@@ -0,0 +1,19 @@
+async function deleteSS() {
+    var secret = document.querySelector("#secret").value
+    const delfile = await fetch(url, {
+        method: "DELETE",
+        headers: { "secret": `${secret}`}
+    })
+    console.log(delfile)
+
+    if(delfile.status === 200) {
+        var error = document.getElementById("error");
+        document.querySelector("#success").innerHTML = "<p>Success!</p>"
+        error.style.display = "none";
+        window.location.replace("http://localhost:5000");
+    } else if (delfile.status === 403) {
+        var success = document.getElementById("success");
+        document.querySelector("#error").innerHTML = "<p>Failure :(<br>That file either doesn't exist or the secret supplied was incorrect.</p>"
+        success.style.display = "none";
+    }
+}
