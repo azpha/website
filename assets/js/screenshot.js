c1e1a2376d95f42feb3afc10c722b0e70ca0720e
Alex / Awex <alex@plaguecraft.xyz>
2021-06-26 03:06:32 -0400

    updated screenshot js file

--- assets/js/screenshot.js
+++ assets/js/screenshot.js
@@ -1,31 +1,66 @@
-async function deleteSS() {
-    var secret = document.querySelector("#secret").value
-    const delfile = await fetch(url, {
-        method: "DELETE",
-        headers: { "secret": `${secret}`}
-    })
-    .then(function(response) {
-        console.log(response.status); // Will show you the status
-        if(response.status === 200) {
-            var error = document.getElementById("error");
-            var success = document.getElementById("success");
-            document.querySelector("#success").innerHTML = "<p>Success! The photo will be viewable until you leave this page.</p>"
-            success.style.display = "block";
-            error.style.display = "none";
-        } else if (response.status === 403) {
-            var success = document.getElementById("success");
-            var error = document.getElementById("error");
-            document.querySelector("#error").innerHTML = `<p>Failed to issue DELETE request<br>Response code: ${response.status}.<br>Double check your token.</p>`
-            success.style.display = "none";
-            error.style.display = "block";
+let url = window.location.href
+async function getFileUrl() {
+    const params = new URLSearchParams(window.location.search)
+    let title = params.get('photo')
+    let apiurl = `http://localhost:1337/screenshots?title=${title}`
+
+        if(title) {
+            try{
+    const { response } = await fetch(apiurl).then(response => response.json());
+    let date = response.date;
+    let photourl = response.directurl;
+    var dateStr = new Date();
+
+    document.querySelector('meta[name="img"]').setAttribute("content", photourl)
+    document.querySelector('meta[name="title"]').setAttribute("content", `Awex's screenshot from ${dateStr}`)
+    document.querySelector('meta[name="url"]').setAttribute("content", url)        
+
+    document.getElementById("title").innerHTML = (`Screenshot from ${date}`)
+    document.getElementById("date").innerHTML = (`<h3>Screenshot from:<br> ${date}</h3>`)
+    document.getElementById("jumbo-internal").innerHTML = "<img src=" + photourl + ">"
+            }
+            catch(error) {
+                console.log('There was an error! ', error)
+                const body = document.getElementById("body")
+                body.style.display = "none";
+                const err = document.getElementById("fourohfour");
+                err.style.display = "block";
+            }
+        } else if (!title) {
+            const body = document.getElementById("body")
+            body.style.display = "none";
+            const err = document.getElementById("fourohfour");
+            err.style.display = "block";
         }
-      })
-}
-
-function showForm() {
-    var form = document.getElementById("form")
-    form.style.display = "block";
-
-    var button = document.getElementById("del")
-    button.style.display = "none";
     }
+
+    async function deleteSS() {
+        var secret = document.querySelector("#secret").value
+        const delfile = await fetch(url, {
+            method: "DELETE",
+            headers: { "secret": `${secret}`}
+        })
+        .then(function(response) {
+            console.log(response.status); // Will show you the status
+            if(response.status === 200) {
+                var error = document.getElementById("err");
+                error.style.display = "none";
+                var success = document.getElementById("success");
+                success.style.display = "block";
+            } else if (response.status === 403 || response.status === 404) {
+                var success = document.getElementById("success");
+                success.style.display = "none";
+                var error = document.getElementById("err");
+                error.style.display = "block";
+                var code = document.getElementById("code").innerHTML = response.status
+            }
+          })
+    }
+    
+    function showForm() {
+        var form = document.getElementById("form")
+        form.style.display = "block";
+    
+        var button = document.getElementById("del")
+        button.style.display = "none";
+        }
