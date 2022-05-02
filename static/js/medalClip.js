bce362c4308f2032c3338f3a77a58e20395654d6
awex 3 <hi@thatalex.dev>
2022-05-02 03:46:31 -0400

    lil reworking + link fixes

--- static/js/medalClip.js
+++ static/js/medalClip.js
@@ -7,5 +7,6 @@ fetch("https://developers.medal.tv/v1/latest?userId=215577&limit=1", {
     }
 }).then(async function(response) {
     const data = await response.json();
-    clipSpan.innerHTML = `${data.contentObjects[0].embedIframeCode}<br>${data.contentObjects[0].contentTitle}`;
+    if(screen.width < 1920) { clipSpan.innerHTML = `<h3><a href="${data.contentObjects[0].directClipUrl}" target="_blank">${data.contentObjects[0].contentTitle}</a></h3>` }
+    else clipSpan.innerHTML = `${data.contentObjects[0].embedIframeCode}<br><hr><h3>${data.contentObjects[0].contentTitle}</h3>`;
 });
