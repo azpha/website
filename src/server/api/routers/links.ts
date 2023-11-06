48fb8f644f55134d451a5912f6731f705aa0931a
alex 3 <alex@alexavfrantz.com>
2023-11-05 23:56:56 -0500

    remove type from link mutation

--- src/server/api/routers/links.ts
+++ src/server/api/routers/links.ts
@@ -10,16 +10,14 @@ export const linksRouter = createTRPCRouter({
         .input(
             z.object({
                 name: z.string().min(1),
-                url: z.string().min(1),
-                type: z.string().min(1)
+                url: z.string().min(1)
             })
         )
         .mutation(async ({ctx, input}) => {
             return ctx.db.socialLink.create({
                 data: {
                     name: input.name,
-                    url: input.url,
-                    type: input.type
+                    url: input.url
                 }
             })
         }),
