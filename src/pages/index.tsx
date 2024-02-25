b7915c1f3a11eeb7e3675cb5e5dc30754ad0bb72
alex 3 <alex@alexavfrantz.com>
2024-02-24 20:43:12 -0500

    fix build complaints

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
                             <p>Michaelsoft Binbows [Version 10.0.23420.6969]</p>
                             <p>(c) Pixelpeek Development. No Rights Reserved.</p>
                             <br />
-                            <p>C:\Users\CoolGuy>whoami</p>
+                            <p>C:\Users\CoolGuy{'>'}whoami</p>
                             <p>Full Name: <span className="font-bold">Alex Frantz</span></p>
                             <p>Based in: <span className="font-bold">Western New York</span></p>
                             <p>Working at: <span className="font-bold hover:text-medal underline"><Link target="_blank" href="https://medal.tv">Medal.tv</Link></span> as a <span className="font-bold">QA Engineer</span></p>
@@ -46,7 +46,7 @@ export default function Home() {
                             <p>Learning: <span className="font-bold">Network Engineering</span>, <span className="font-bold">Game Dev</span>, <span className="font-bold">Frontend</span>, <span className="font-bold">Backend</span></p>
                             <p>Website: <span className="font-bold underline hover:text-gray-500"><Link target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">alexavfrantz.com</Link></span></p>
                             <br />
-                            <p>C:\Users\CoolGuy></p>
+                            <p>C:\Users\CoolGuy{'>'}</p>
                         </div>
                     </div>
                 </div>
