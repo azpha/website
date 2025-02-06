03ea03785c8f0323df8e2728787fbbccf39515eb
Alex Frantz <alex@alexav.gg>
2025-02-05 20:26:15 -0500

    add routes + 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/index';
 import PrivacyPage from './pages/privacy';
 import MusicWidget from './pages/music';
 import LinksPage from './pages/links';
+import NotFound from './pages/NotFound';
 
 import './assets/main.css'
 
@@ -12,43 +13,9 @@ export default function App() {
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/privacy" element={<PrivacyPage />} />
-      <Route path="*" element={<div>404 Not Found</div>} />
+      <Route path="/music" element={<MusicWidget />} />
+      <Route path="/links" element={<LinksPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
-
-// import ReactDOM from 'react-dom/client'
-// import {
-//   createBrowserRouter,
-//   RouterProvider
-// } from "react-router-dom"
-// import './assets/main.css'
-
-// // pages
-// import MainPage from './pages/index';
-// import PrivacyPage from './pages/privacy';
-// import MusicWidget from './pages/music';
-// import LinksPage from './pages/links';
-
-// // const router = createBrowserRouter([
-// //   {
-// //     path: '/',
-// //     element: <MainPage />
-// //   },
-// //   {
-// //     path: '/privacy',
-// //     element: <PrivacyPage />
-// //   },
-// //   {
-// //     path: '/music',
-// //     element: <MusicWidget />
-// //   },
-// //   {
-// //     path: '/links',
-// //     element: <LinksPage />
-// //   }
-// // ])
-
-// // ReactDOM.createRoot(document.getElementById('root')!).render(
-// //   <RouterProvider router={router} />
-// // )
