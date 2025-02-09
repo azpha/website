26ee2b000aae6b5b1e87c6a9888aaa4006ccfc0c
Alex Frantz <alex@alexav.gg>
2025-02-09 14:35:48 -0500

    add image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'storage.alexav.gg',
+      'cdn.medal.tv',
+      'lastfm.freetls.fastly.net'
+    ]
+  }
 };
 
 export default nextConfig;
